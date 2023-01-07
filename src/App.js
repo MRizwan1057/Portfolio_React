813e8d4e091ade8dc5e7a40c5397881993cc20c0
MRizwan1057 <mrizwanr202@gmail.com>
2023-01-07 05:01:34 +0500

    footer done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Experience from "./pages/Experience";
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-import {
-  FaGithubSquare,
-  FaLinkedinIn,
-  FaWhatsappSquare,
-  FaEnvelopeSquare,
-} from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaWhatsapp, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div>
-          <h5>Copyright © 2023 All Rights Reserved by .</h5>
-        </div>
-        <div>
-          <FaGithubSquare />
-          <FaLinkedinIn />
-          <FaWhatsappSquare />
-          <FaEnvelopeSquare />
+      <footer className="footer fixed-bottom mb-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 mx-auto text-center">
+              <div className="footer_icons mb-4 ">
+                <FaGithub className="" />
+                <FaLinkedinIn className=" " />
+                <FaWhatsapp className=" " />
+                <FaEnvelope className=" " />
+              </div>
+              <div>
+                <p>Copyright © 2023 All Rights Reserved by .</p>
+              </div>
+            </div>
+          </div>
         </div>
       </footer>
     </>
